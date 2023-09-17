91947b2f979260b07d8a9dc5cc8c15e5bc8b9f57
Alvaro-SP <3034161730108@ingenieria.usac.edu.gt>
2023-09-16 19:35:55 -0600

    added the sell DB

--- scriptsSQL/ventasSQLserver.sql
+++ scriptsSQL/ventasSQLserver.sql
@@ -1,20 +1,61 @@
-CREATE DATABASE comprasETLsemi2;
+CREATE DATABASE ventasETLsemi2;
 
-USE comprasETLsemi2;
+USE ventasETLsemi2;
 
 CREATE TABLE Temporal (
-    
+    Fecha VARCHAR(50),
+    CodigoCliente VARCHAR(50),
+    NombreCliente VARCHAR(100),
+    TipoCliente VARCHAR(50),
+    DireccionCliente VARCHAR(200),
+    NumeroCliente VARCHAR(50),
+    CodVendedor VARCHAR(50),
+    NombreVendedor VARCHAR(100),
+    Vacacionista VARCHAR(50),
+    CodProducto VARCHAR(50),
+    NombreProducto VARCHAR(100),
+    MarcaProducto VARCHAR(50),
+    Categoria VARCHAR(50),
+    SodSuSursal VARCHAR(50),
+    NombreSucursal VARCHAR(100),
+    DireccionSucursal VARCHAR(200),
+    Region VARCHAR(50),
+    Departamento VARCHAR(50),
+    Unidades VARCHAR(50),
+    PrecioUnitario VARCHAR(50),
 )
 
-CREATE TABLE Compras(
-    
+CREATE TABLE Ventas(
+    Fecha VARCHAR(50),
+    CodigoCliente VARCHAR(50),
+    NombreCliente VARCHAR(100),
+    TipoCliente VARCHAR(50),
+    DireccionCliente VARCHAR(200),
+    NumeroCliente VARCHAR(50),
+    CodVendedor VARCHAR(50),
+    NombreVendedor VARCHAR(100),
+    Vacacionista VARCHAR(50),
+    CodProducto VARCHAR(50),
+    NombreProducto VARCHAR(100),
+    MarcaProducto VARCHAR(50),
+    Categoria VARCHAR(50),
+    SodSuSursal VARCHAR(50),
+    NombreSucursal VARCHAR(100),
+    DireccionSucursal VARCHAR(200),
+    Region VARCHAR(50),
+    Departamento VARCHAR(50),
+    Unidades VARCHAR(50),
+    PrecioUnitario VARCHAR(50),
 )
 
 SELECT * FROM Temporal;
-SELECT * FROM Compras;
+SELECT * FROM Ventas;
 
 DELETE FROM Temporal;
-DELETE FROM Compras;
+DELETE FROM Ventas;
 
 DROP TABLE Temporal;
-DROP TABLE Compras;
+DROP TABLE Ventas;
+
+
+
